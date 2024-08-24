151632340dc4bb0c7ff337698dc7df1fea2cf6f0
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-08-24 22:01:57 +0700

    Refactored fetchWeatherData to accept parameters. Streamlined icon mapping, added dynamic unit display, enhanced date display, and reorganized displayCurrentWeather.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let locationQuery = "Bandung";
+let searchLocation = "Bandung";
 let measurementUnit = "metric";
-
 const apiKey = "G8796BZ69XEMGGGRH42HX5RSQ";
 
-const fetchWeatherData = async (locationQuery) => {
+const fetchWeatherData = async (searchLocation, measurementUnit) => {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationQuery}?unitGroup=${measurementUnit}&key=${apiKey}`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchLocation}?unitGroup=${measurementUnit}&key=${apiKey}`
     );
 
     if (response.status !== 200) {
@@ -24,115 +23,183 @@ const updateMeasurementUnit = () => {
   const measurementUnitSelector = document.querySelector(
     ".measurement-unit-selector"
   );
-  measurementUnit = measurementUnitSelector.value;
+  return (measurementUnit = measurementUnitSelector.value);
 };
 
-const updateLocationQuery = () => {
+const setDegreeSymbol = () => {
+  const measurementUnit = updateMeasurementUnit();
+  const degreeSymbol = measurementUnit === "us" ? "°F" : "°C";
+  return degreeSymbol;
+};
+
+const setSpeedUnit = () => {
+  const measurementUnit = updateMeasurementUnit();
+  const speedUnit = measurementUnit === "metric" ? "km/h" : "mph";
+  return speedUnit;
+};
+
+const updateSearchLocation = () => {
   const locationSearchInput = document.querySelector(".location-search-input");
-  locationQuery = locationSearchInput.value;
+  searchLocation = locationSearchInput.value;
 };
 
-const getWeatherData = async () => {
-  const weatherData = await fetchWeatherData(locationQuery);
+const prepareWeatherData = async () => {
+  const weatherData = await fetchWeatherData(searchLocation, measurementUnit);
 
   const location = weatherData.resolvedAddress;
   const timezone = weatherData.timezone;
   const description = weatherData.description;
   const conditions = weatherData.currentConditions.conditions;
+  const dateTime = weatherData.currentConditions.datetime;
   const humidity = weatherData.currentConditions.humidity;
   const icon = weatherData.currentConditions.icon;
   const temperature = weatherData.currentConditions.temp;
+  const windSpeed = weatherData.currentConditions.windspeed;
+  const sunrise = weatherData.currentConditions.sunrise;
+  const sunset = weatherData.currentConditions.sunset;
+  const days = weatherData.days;
 
   return {
     location,
     timezone,
     description,
     conditions,
+    dateTime,
     humidity,
     icon,
     temperature,
+    windSpeed,
+    sunrise,
+    sunset,
+    days,
   };
 };
 
 const setWeatherIcon = (weatherData) => {
-  const weatherIcon = document.querySelector(".weather-icon");
+  const weatherIconMapping = {
+    "clear-day": "assets/clear-day.svg",
+    "clear-night": "assets/clear-night.svg",
+    cloudy: "assets/cloudy.svg",
+    fog: "assets/fog.svg",
+    hail: "assets/hail.svg",
+    "partly-cloudy-day": "assets/partly-cloudy-day.svg",
+    "partly-cloudy-night": "assets/partly-cloudy-night.svg",
+    "rain-snow-showers-day": "assets/rain-snow-showers-day.svg",
+    "rain-snow-showers-night": "assets/rain-snow-showers-night.svg",
+    "rain-snow": "assets/rain-snow.svg",
+    rain: "assets/rain.svg",
+    "showers-day": "assets/showers-day.svg",
+    "showers-night": "assets/showers-night.svg",
+    sleet: "assets/sleet.svg",
+    "snow-showers-day": "assets/snow-showers-day.svg",
+    "snow-showers-night": "assets/snow-showers-night.svg",
+    snow: "assets/snow.svg",
+    "thunder-rain": "assets/thunder-rain.svg",
+    "thunder-showers-day": "assets/thunder-showers-day.svg",
+    "thunder-showers-night": "assets/thunder-showers-night.svg",
+    thunder: "assets/thunder.svg",
+    wind: "assets/wind.svg",
+  };
 
-  switch (weatherData.icon) {
-    case "clear-day":
-      return (weatherIcon.src = "assets/clear-day.svg");
-    case "clear-night":
-      return (weatherIcon.src = "assets/clear-night.svg");
-    case "cloudy":
-      return (weatherIcon.src = "assets/cloudy.svg");
-    case "fog":
-      return (weatherIcon.src = "assets/fog.svg");
-    case "hail":
-      return (weatherIcon.src = "assets/hail.svg");
-    case "partly-cloudy-day":
-      return (weatherIcon.src = "assets/partly-cloudy-day.svg");
-    case "partly-cloudy-night":
-      return (weatherIcon.src = "assets/partly-cloudy-night.svg");
-    case "rain-snow-showers-day":
-      return (weatherIcon.src = "assets/rain-snow-showers-day.svg");
-    case "rain-snow-showers-night":
-      return (weatherIcon.src = "assets/rain-snow-showers-night.svg");
-    case "rain-snow":
-      return (weatherIcon.src = "assets/rain-snow.svg");
-    case "rain":
-      return (weatherIcon.src = "assets/rain.svg");
-    case "showers-day":
-      return (weatherIcon.src = "assets/showers-day.svg");
-    case "showers-night":
-      return (weatherIcon.src = "assets/showers-night.svg");
-    case "sleet":
-      return (weatherIcon.src = "assets/sleet.svg");
-    case "snow-showers-day":
-      return (weatherIcon.src = "assets/snow-showers-day.svg");
-    case "snow-showers-night":
-      return (weatherIcon.src = "assets/snow-showers-night.svg");
-    case "snow":
-      return (weatherIcon.src = "assets/snow.svg");
-    case "thunder-rain":
-      return (weatherIcon.src = "assets/thunder-rain.svg");
-    case "thunder-showers-day":
-      return (weatherIcon.src = "assets/thunder-showers-day.svg");
-    case "thunder-showers-night":
-      return (weatherIcon.src = "assets/thunder-showers-night.svg");
-    case "thunder":
-      return (weatherIcon.src = "assets/thunder.svg");
-    case "wind":
-      return (weatherIcon.src = "assets/wind.svg");
-    default:
-      return null;
+  const weatherIcon = document.querySelector(".weather-icon");
+  weatherIcon.src = weatherIconMapping[weatherData.icon] || "";
+};
+
+const displayCurrentDate = () => {
+  const currentDate = new Date();
+  const dayOfWeeks = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const todayDay = dayOfWeeks[currentDate.getDay()];
+  const todayDate = currentDate.getDate();
+  const todayMonth = currentDate.getMonth();
+  const todayYear = currentDate.getFullYear();
+
+  const dayMonthYear = document.querySelector(".day-month-year");
+  dayMonthYear.innerHTML = `${todayDay}, ${todayDate} ${todayMonth} ${todayYear}`;
+};
+
+const displayCurrentWeather = async (weatherData) => {
+  const location = document.querySelector(".location");
+  location.innerHTML = weatherData.location;
+
+  const timezone = document.querySelector(".timezone");
+  timezone.innerHTML = `Timezone: ${weatherData.timezone}`;
+
+  const description = document.querySelector(".description");
+  description.innerHTML = weatherData.description;
+
+  const conditions = document.querySelector(".conditions");
+  conditions.innerHTML = weatherData.conditions;
+
+  const dateTime = document.querySelector(".date-time");
+  dateTime.innerHTML = `Time: ${weatherData.dateTime}`;
+
+  const humidity = document.querySelector(".humidity");
+  humidity.innerHTML = `Humidity: ${weatherData.humidity}%`;
+
+  const temperature = document.querySelector(".temperature");
+  const degreeSymbol = setDegreeSymbol();
+  temperature.innerHTML = `${weatherData.temperature} ${degreeSymbol}`;
+
+  const windSpeed = document.querySelector(".wind-speed");
+  const speedUnit = setSpeedUnit();
+  windSpeed.innerHTML = `Wind Speed: ${weatherData.windSpeed} ${speedUnit}`;
+
+  const sunrise = document.querySelector(".sunrise");
+  sunrise.innerHTML = `Sunrise: ${weatherData.sunrise}`;
+
+  const sunset = document.querySelector(".sunset");
+  sunset.innerHTML = `Sunset: ${weatherData.sunset}`;
+};
+
+const resetForecastCards = () => {
+  const forecastDashboard = document.querySelector(".forecast-dashboard");
+  forecastDashboard.innerHTML = "";
+};
+
+const generateForecastCards = () => {
+  const forecastDashboard = document.querySelector(".forecast-dashboard");
+
+  for (let i = 0; i < 10; i++) {
+    const forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+    forecastCard.innerHTML = "TEST";
+    forecastDashboard.appendChild(forecastCard);
   }
 };
 
-const displayWeatherData = async (weatherData) => {
-  const location = document.querySelector(".location");
-  location.textContent = weatherData.location;
-
-  const timezone = document.querySelector(".timezone");
-  timezone.textContent = `Timezone: ${weatherData.timezone}`;
-
-  const description = document.querySelector(".description");
-  description.textContent = weatherData.description;
-
-  const conditions = document.querySelector(".conditions");
-  conditions.textContent = weatherData.conditions;
-
-  const humidity = document.querySelector(".humidity");
-  humidity.textContent = `Humidity: ${weatherData.humidity}`;
-
-  const temperature = document.querySelector(".temperature");
-  temperature.textContent = weatherData.temperature;
+const displayForecasts = async (weatherData) => {
+  console.log(weatherData.days);
 };
+
+const loadDefaultState = async () => {
+  const weatherData = await prepareWeatherData();
+  displayCurrentDate();
+  setWeatherIcon(weatherData);
+  displayCurrentWeather(weatherData);
+  generateForecastCards();
+  displayForecasts(weatherData);
+};
+
+document.addEventListener("DOMContentLoaded", loadDefaultState);
 
 const locationSearchForm = document.querySelector(".location-search-form");
 locationSearchForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   updateMeasurementUnit();
-  updateLocationQuery();
-  const weatherData = await getWeatherData();
+  updateSearchLocation();
+  const weatherData = await prepareWeatherData();
+  displayCurrentDate();
   setWeatherIcon(weatherData);
-  displayWeatherData(weatherData);
+  displayCurrentWeather(weatherData);
+  resetForecastCards();
+  generateForecastCards();
+  displayForecasts(weatherData);
 });
